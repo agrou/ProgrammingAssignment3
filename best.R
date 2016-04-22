27cd654e63446f241325287719d02a01c552b466
agrou <ac.ilustat@gmail.com>
2016-04-22 16:00:29 +0100

    Added files via upload

--- best.R
+++ best.R
@@ -2,13 +2,14 @@ library(dplyr)
 library(tidyr)
 library(magrittr)
 
-##Finding the best hospital in a state
+## Finding the best hospital in a state
+
 best <- function(state, outcome){
-        ##Read outcome data
+        ## Read outcome data
         hosp_dat <- read.csv("outcome-of-care-measures.csv", 
                              na.strings = "Not Available", 
                              stringsAsFactors = FALSE) %>%
-                #Select the data that is needed
+                # Select the data that is needed
                 select(State, starts_with("Hospital")) %>% 
                 select(-contains("Readmission")) %>%
                 gather(Outcome, Rate, contains("Rate")) %>%
@@ -16,22 +17,22 @@ best <- function(state, outcome){
                                                gsub("Hospital.30.Day.Death..Mortality..Rates.from.", "", 
                                                     Outcome)))) %>%
                 select(-Outcome) %>%
-                #Remove missing values
+                # Remove missing values
                 filter(!is.na(Rate)) %>% 
-                #Organise data by State, Outcome and Hospital Name
+                # Organise data by State, Outcome and Hospital Name
                 group_by(State, Outcome2, Hospital.Name) %>% 
-                #Get the minimum values for the hospital Rate
+                # Get the minimum values for the hospital Rate
                 summarise(minRate = min(Rate)) %>%
-                #Reorganize the values of Rate by hospital name
+                # Reorganize the values of Rate by hospital name
                 arrange(State, Outcome2, minRate, Hospital.Name)
         
-        ##Check if state and outcome are valid
+        ## Check if state and outcome are valid
         
         if (!(state %in% unique(hosp_dat$State))) stop("Invalid state")
         
         if (!(outcome %in% unique(hosp_dat$Outcome2))) stop("Invalid outcome")
         
-        ##Then select the right hospital 
+        ## Then select the right hospital 
         hosp_r <- hosp_dat %>% 
                 filter(State == state & Outcome2 == outcome) %>% 
                 summarise(fmin = first(Hospital.Name)) 
@@ -39,6 +40,7 @@ best <- function(state, outcome){
         print(hosp_min[[3]]) 
 }
 
+# Testing the function
 best("TX", "heart attack")
 best("TX", "heart failure")
 best("MD", "pneumonia")
@@ -46,4 +48,4 @@ best("BB", "hert attack")
 best("NY", "hert attack")
 
 
-# Function best
+
